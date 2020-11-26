a25dc9d86cff6cf9fdff4ffd4a4e990a5a8a2c3d
Daniel Tsai <idaniel.twc@gmail.com>
2020-11-26 15:14:59 +0800

    fix(Connectivity): modify CCA delay time

--- Connectivity/CC1101_MSP430.c
+++ Connectivity/CC1101_MSP430.c
@@ -517,7 +517,7 @@ uint8_t transmit(void)
     {
         GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0);      // Red LED on
         // spi_write_strobe(SFTX);    //flush TX Buffer
-        vTaskDelay(rand() % 6 * 40);
+        vTaskDelay(rand() % 6 * 100);
         if (debug_level)
             print2uart("CCA\n");
     } else
